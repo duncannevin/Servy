6c34057abc8971dba0371e768d71cf0df3aa6851
duncannevin <duncan.nevin@gmail.com>
2023-03-04 10:55:19 -0800

    Add kickstart server.

---
+++ lib/servy/kickstarter.ex
@@ -0,0 +1,32 @@
+defmodule Servy.Kickstarter do
+  use GenServer
+
+  @name :http_server
+
+  def start do
+    IO.puts "Starting the kickstarter..."
+    GenServer.start(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def init(:ok) do
+    Process.flag(:trap_exit, true)
+    IO.puts "Starting the HTTP server..."
+    server_pid = start_server()
+    {:ok, server_pid}
+  end
+
+  def handle_info({:EXIT, _pid, reason}, _state) do
+    IO.puts "HttpServer exited (#{inspect reason})"
+    server_pid = start_server()
+    {:noreply, server_pid}
+  end
+
+  def start_server do
+    IO.puts "Starting the HTTP server..."
+    server_pid = spawn_link(Servy.HttpServer, :start, [8080])
+    Process.register(server_pid, @name)
+    server_pid
+  end
+
+  def get_server, do: Process.whereis(@name)
+end
